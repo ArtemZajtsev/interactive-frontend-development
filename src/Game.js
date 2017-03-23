853cad5bab832baac47ad93e186ff490bfeaea7e
Artem Zaitsev <minhiriath94@gmail.com>
2017-03-23 14:59:51 +0200

    Game class implemented

--- src/Game.js
+++ src/Game.js
@@ -1,3 +1,21 @@
 /**
  * Created by minhi_000 on 23.03.2017.
  */
+class Game {
+
+    constructor() {
+        this.targetNumber = Math.floor(Math.random()*10);
+    }
+
+    makeGuess(number){
+        if(number > this.targetNumber){
+            return "was greater than target";
+        }
+        else if (number < this.targetNumber){
+            return "was lower than target";
+        }
+        else return "was correct";
+    }
+}
+
+export default Game;
