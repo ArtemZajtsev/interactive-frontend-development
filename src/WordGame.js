6dc9baa1bc7f90d3a756264707ecde3113fdfd5b
Artem Zaitsev <minhiriath94@gmail.com>
2017-03-26 15:41:44 +0300

    word game logic

---
+++ src/WordGame.js
@@ -0,0 +1,44 @@
+class WordGame {
+
+    constructor() {
+        this.wordsArray = ['paper', 'grill', 'basil', 'hinge', 'ruler'];
+        this.targetWord = this.wordsArray[Math.floor(Math.random() * this.wordsArray.length)]
+        this.win = false;
+    }
+
+    makeGuess(guessString) {
+        let regex = new RegExp(guessString, 'g');
+        if (regex.test(this.targetWord)) {
+            if (guessString === this.targetWord) {
+                this.win = true;
+            }
+            else {
+                let matches = [];
+                let match = regex.exec(this.targetWord);
+                while (match != null) {
+                    matches.push(match);
+                    match = regex.exec(this.targetWord);
+                }
+                return matches;
+            }
+        }
+        else return false;
+    }
+
+    isWin() {
+        return this.win;
+    }
+}
+
+// var myString = "pewpewpew";
+// var matches = [];
+// var myRegexp = /p/g;
+// match = myRegexp.exec(myString);
+// while (match != null) {
+//     // matched text: match[0]
+//     // match start: match.index
+//     // capturing group n: match[n]
+//     matches.push(match);
+//     match = myRegexp.exec(myString);
+// }
+// console.log(matches)
