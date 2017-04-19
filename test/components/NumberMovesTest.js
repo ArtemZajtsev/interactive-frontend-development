8f341a92c8b51e38714b20541cbc566728d5b1a2
Artem Zaitsev <minhiriath94@gmail.com>
2017-04-19 15:09:42 +0300

    test fixes

--- test/components/NumberMovesTest.js
+++ test/components/NumberMovesTest.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import NumberMoves from '../../src/components/NumberMoves';
+import {EQ, EQT, GT, GTT, LT, LTT} from '../../src/constants'
 
 describe('NumberMoves', () => {
     it('renders', () => {
         expect(shallow(
             <NumberMoves moves={[{
                 guess: 'test',
-                id: 1,
-                text: 'was correct'
+                comparedToAnswer: EQ
             }]}></NumberMoves>
         )).to.exist;
     });
@@ -16,25 +16,46 @@ describe('NumberMoves', () => {
         expect(shallow(
             <NumberMoves moves={[{
                 guess: 'test',
-                id: 1,
-                text: 'was correct'
+                comparedToAnswer: EQ
             }]}></NumberMoves>
-        )).to.include.text('was correct');
+        )).to.include.text(EQT);
     });
-    it('has green color if text == was correct', () => {
+    it('has green color if EQ', () => {
         expect(shallow(
             <NumberMoves moves={[{
                 guess: 'test',
-                id: 1,
-                text: 'was correct'
+                comparedToAnswer: EQ
             }]}></NumberMoves>).find('p').hasClass('green')).to.equal(true);
     });
     it('has red color if text != was correct', () => {
         expect(shallow(
             <NumberMoves moves={[{
                 guess: 'test',
-                id: 1,
-                text: 'test'
+                comparedToAnswer: GT
             }]}></NumberMoves>).find('p').hasClass('red')).to.equal(true);
+    });
+    it('shows was correct is guess was correct', () => {
+        expect(shallow(
+            <NumberMoves moves={[{
+                guess: 'test',
+                comparedToAnswer: EQ
+            }]}></NumberMoves>
+        )).to.include.text(EQT);
+    });
+    it('shows was greater then if guess was greater then actual', () => {
+        expect(shallow(
+            <NumberMoves moves={[{
+                guess: 'test',
+                comparedToAnswer: GT
+            }]}></NumberMoves>
+        )).to.include.text(GTT);
+    });
+    it('shows was lower then if guess was lower then actual', () => {
+        expect(shallow(
+            <NumberMoves moves={[{
+                guess: 'test',
+                comparedToAnswer: LT
+            }]}></NumberMoves>
+        )).to.include.text(LTT);
     });
 });
--- test/components/WordMovesTest.js
+++ test/components/WordMovesTest.js
@@ -6,35 +6,35 @@ describe('WordMoves', () => {
     it('renders', () => {
         expect(shallow(
             <WordMoves moves={[{
-                guess: 'test',
-                id: 1,
-                correct: []
+                correct: false,
+                letterMatches:[],
+                guess:'pew'
             }]}></WordMoves>
         )).to.exist;
     });
     it('renders contained element', () => {
         expect(shallow(
             <WordMoves moves={[{
-                guess: 'test',
-                id: 1,
-                correct: []
+                correct: false,
+                letterMatches:[],
+                guess:'pew'
             }]}></WordMoves>
-        )).to.include.text('test');
+        )).to.include.text('pew');
     });
     it('renders correct leters in green', () => {
         expect(shallow(
             <WordMoves moves={[{
-                guess: 'test',
-                id: 1,
-                correct: [0]
+                correct: false,
+                letterMatches:[true],
+                guess:'pew'
             }]}></WordMoves>).find('span').first().hasClass('green')).to.equal(true);
     });
     it('renders incorrect leters in red', () => {
         expect(shallow(
             <WordMoves moves={[{
-                guess: 'test',
-                id: 1,
-                correct: [3]
+                guess: 'pew',
+                correct:false,
+                letterMatches: [false]
             }]}></WordMoves>).find('span').first().hasClass('red')).to.equal(true);
     });
 });
